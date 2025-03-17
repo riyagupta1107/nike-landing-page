8ebd6d15dc0451bdcd66d38eb552f5cff7a24df2
riyagupta1107 <riya.gupta070705@gmail.com>
2025-03-17 20:16:06 +0530

    app set-up and navigation bar

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from './Hero';
+import PopularProducts from './PopularProducts';
+import Footer from './Footer';
+import Services from './Services';
+import Subscribe from './Subscribe';
+import SuperQuality from './SuperQuality';
+import CustomerReviews from './CustomerReviews';
+import SpecialOffer from './SpecialOffer';
+
+export {
+    Hero,
+    PopularProducts,
+    CustomerReviews,
+    Subscribe,
+    SuperQuality,
+    SpecialOffer,
+    Services,
+    Footer
+}
